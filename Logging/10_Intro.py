747c4a5e584b5d0b447cf4a6787cea354d72b585
Oleg <oleg.smolanko@adesso.de>
2021-05-13 15:06:32 +0200

    changes

--- Logging/10_Intro.py
+++ Logging/10_Intro.py
@@ -5,6 +5,7 @@ import logging
 
 # https://www.youtube.com/watch?v=-ARI4Cz-awo
 
+# https://fangpenlin.com/posts/2012/08/26/good-logging-practice-in-python/
 
 # By default, there are 5 standard levels indicating the severity of events. 
 # Each has a corresponding method that can be used to log events at that level of severity. 
